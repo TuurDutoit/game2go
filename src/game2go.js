a97268b692307518d11998ef1f5daeacd265dd26
TuurDutoit <tuur.dutoit@live.nl>
2014-04-28 17:47:47 +0200

    Basic setup

---
+++ src/game2go.js
@@ -0,0 +1,13 @@
+var Game;
+
+//(function() { //Mask everything but Game object
+
+
+Game = function(elem, options) {
+	
+}
+
+
+
+
+//})();
---
+++ test/levels.js
@@ -0,0 +1,10 @@
+var levels = [
+	[ //Level 1
+		[ //Column 1
+			function(d) {
+				d.fillStyle("#FF0000");
+				d.rect(20, 20, 20, 20);
+			}
+		]
+	]
+]
