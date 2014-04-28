38a01ba692941e466fb242b55311b14628a20103
TuurDutoit <tuur.dutoit@live.nl>
2014-04-28 18:24:29 +0200

    Path, stroke and fill functions on Draw

--- src/game2go.js
+++ src/game2go.js
@@ -35,6 +35,22 @@ Draw.prototype.strokeWidth = function(width) {
 	this.context.strokeWidth = width;
 	return this;
 }
+Draw.prototype.beginPath = function() {
+	this.context.beginPath();
+	return this;
+}
+Draw.prototype.endPath = function() {
+	this.context.endPath();
+	return this;
+}
+Draw.prototype.stroke = function() {
+	this.context.stroke();
+	return this;
+}
+Draw.prototype.fill = function() {
+	this.context.fill();
+	return this;
+}
 
 Draw.prototype.fillRect = function(x, y, w, h) {
 	this.context.fillRect(x+this.offsetX, y+this.offsetY, w, h);
@@ -49,6 +65,7 @@ Draw.rect = function(x, y, w, h) {
 	this.fillRect(x, y, w, h);
 	return this;
 }
+
 
 
 
