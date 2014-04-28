f8745fc456e17d6dc22ef7b6b218ca24f35280b1
TuurDutoit <tuur.dutoit@live.nl>
2014-04-28 18:09:59 +0200

    Added Draw with rect functions

--- src/game2go.js
+++ src/game2go.js
@@ -8,12 +8,46 @@ Game = function(elem, options) {
 	self.options = options;
 	self.elem    = elem;
 	self.context = self.elem.getContext("2d");
-	//self.Draw    = new Draw(this.context);
+	self.Draw    = new Draw(this.context);
 
 	self.world   = [];
 	self.level   = [];
 	self.buffer  = [];
 
+}
+
+
+var Draw = function(context, offsetX, offsetY) {
+	this.context = context;
+	this.offsetX = offsetX || 0;
+	this.offsetY = offsetY || 0;
+}
+
+Draw.prototype.fillStyle = function(style) {
+	this.context.fillStyle = style;
+	return this;
+}
+Draw.prototype.strokeStyle = function(style) {
+	this.context.strokeStyle = style;
+	return this;
+}
+Draw.prototype.strokeWidth = function(width) {
+	this.context.strokeWidth = width;
+	return this;
+}
+
+Draw.prototype.fillRect = function(x, y, w, h) {
+	this.context.fillRect(x+this.offsetX, y+this.offsetY, w, h);
+	return this;
+}
+Draw.prototype.strokeRect = function(x, y, w, h) {
+	this.context.strokeRect(x+this.offsetX, y+this.offsetY, w, h);
+	return this;
+}
+Draw.rect = function(x, y, w, h) {
+	this.strokeRect(x, y, w, h);
+	this.fillRect(x, y, w, h);
+	return this;
 }
 
 
