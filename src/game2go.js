923642e88d76bfbefeb52dab049a9fabdaf1cebb
TuurDutoit <tuur.dutoit@live.nl>
2014-04-28 19:08:18 +0200

    Added most important functions to Draw

--- src/game2go.js
+++ src/game2go.js
@@ -4,67 +4,166 @@ var Game;
 
 
 Game = function(elem, options) {
-	var self     = this;
-	self.options = options;
-	self.elem    = elem;
-	self.context = self.elem.getContext("2d");
-	self.Draw    = new Draw(this.context);
+    var self     = this;
+    self.options = options;
+    self.elem    = elem;
+    self.context = self.elem.getContext("2d");
+    self.Draw    = new Draw(this.context);
 
-	self.world   = [];
-	self.level   = [];
-	self.buffer  = [];
+    self.world   = [];
+    self.level   = [];
+    self.buffer  = [];
 
+    return self;
 }
+
+
+
 
 
 var Draw = function(context, offsetX, offsetY) {
-	this.context = context;
-	this.offsetX = offsetX || 0;
-	this.offsetY = offsetY || 0;
+    var self     = this;
+    this.context = context;
+    this.offsetX = offsetX || 0;
+    this.offsetY = offsetY || 0;
+
+    return self;
+}
+
+Draw.prototype.beginPath = function() {
+    this.context.beginPath();
+    return this;
+}
+Draw.prototype.closePath = function() {
+    this.context.endPath();
+    return this;
+}
+Draw.prototype.moveTo = function(x, y) {
+    this.context.moveTo(x, y);
+    return this;
+}
+Draw.prototype.save = function() {
+    this.context.save();
+    return this;
+}
+Draw.prototype.restore = function() {
+    this.context.restore();
+    return this;
+}
+Draw.prototype.clip = function() {
+    this.context.clip();
+    return this;
 }
 
 Draw.prototype.fillStyle = function(style) {
-	this.context.fillStyle = style;
-	return this;
+    this.context.fillStyle = style;
+    return this;
 }
 Draw.prototype.strokeStyle = function(style) {
-	this.context.strokeStyle = style;
-	return this;
+    this.context.strokeStyle = style;
+    return this;
 }
 Draw.prototype.strokeWidth = function(width) {
-	this.context.strokeWidth = width;
-	return this;
-}
-Draw.prototype.beginPath = function() {
-	this.context.beginPath();
-	return this;
-}
-Draw.prototype.endPath = function() {
-	this.context.endPath();
-	return this;
+    this.context.strokeWidth = width;
+    return this;
 }
 Draw.prototype.stroke = function() {
-	this.context.stroke();
-	return this;
+    this.context.stroke();
+    return this;
 }
 Draw.prototype.fill = function() {
-	this.context.fill();
-	return this;
+    this.context.fill();
+    return this;
+}
+Draw.prototype.clearRect = function(x, y, w, h) {
+    this.context.clearRect(x+this.offsetX, y+this.offsetY, w, h);
+    return this;
 }
 
 Draw.prototype.fillRect = function(x, y, w, h) {
-	this.context.fillRect(x+this.offsetX, y+this.offsetY, w, h);
-	return this;
+    this.context.fillRect(x+this.offsetX, y+this.offsetY, w, h);
+    return this;
 }
 Draw.prototype.strokeRect = function(x, y, w, h) {
-	this.context.strokeRect(x+this.offsetX, y+this.offsetY, w, h);
-	return this;
+    this.context.strokeRect(x+this.offsetX, y+this.offsetY, w, h);
+    return this;
 }
-Draw.rect = function(x, y, w, h) {
-	this.strokeRect(x, y, w, h);
-	this.fillRect(x, y, w, h);
-	return this;
+Draw.prototype.fullRect = function(x, y, w, h) {
+    this.strokeRect(x+this.offsetX, y+this.offsetY, w, h);
+    this.fillRect(x+this.offsetX, y+this.offsetY, w, h);
+    return this;
 }
+
+Draw.prototype.lineTo = function(x, y) {
+    this.context.lineTo(x+this.offsetX, y+this.offsetY);
+    return this;
+}
+Draw.prototype.rect = function(x, y, w, h) {
+    this.context.rect(x+this.offsetX, y+this.offsetY, w, h);
+    return this;
+}
+Draw.prototype.arc = function(x, y, r, sa, ea, ac) {
+    this.context.arc(x+this.offsetX, y+this.offsetY, r, sa, ea, ac);
+    return this;
+}
+Draw.prototype.arcTo = function(x1, y1, x2, y2, r) {
+    this.context.arcTo(x1+this.offsetX, y1+this.offset.Y, x2+this.offsetX, y2+this.offsetY, r);
+    return this;
+}
+Draw.prototype.bezierCurveTo = function(x1, y1, x2, y2, x3, y3) {
+    this.context.bezierCurveTo(x1+this.offsetX, y1+this.offset.Y, x2+this.offsetX, y2+this.offsetY, x3+this.offsetX, y3+this.offsetY);
+    return this;
+}
+Draw.prototype.quadraticCurveTo = function(x1, y1, x2, y2) {
+    this.context.quadraticCurveTo(x1+this.offsetX, y1+this.offset.Y, x2+this.offsetX, y2+this.offsetY);
+    return this;
+}
+
+Draw.prototype.fillText = function(text, x, y, maxw) {
+    this.context.fillText(text, x+this.offsetX, y+this.offsetY, maxw);
+    return this;
+}
+Draw.prototype.strokeText = function(text, x, y, maxw) {
+    this.context.strokeText(text, x+this.offsetX, y+this.offsetY, maxw);
+    return this;
+}
+Draw.prototype.fullText = function(text, x, y, maxw) {
+    this.fillText(text, x, y, maxw);
+    this.strokeText(text, x, y, maxw);
+    return this;
+}
+
+
+/* NOT IMPLEMETED
+ * ==============*/
+ //->Security
+     //.rotate()
+     //.translate()
+     //.scale()
+     //.scrollPathIntoView()
+ //->Too complex/not enough time --> look into them!
+ //TODO
+     //.createImageData()
+     //.createLinearGradient()
+     //.createRadialGradient()
+     //.createPattern()
+     //.drawCustomFocusRing()
+     //.drawSystemFocusRing()
+     //.drawImage()
+     //.getImageData()
+     //.getLineDash()
+     //.isPointInPath()
+     //.isPointInStroke()
+     //.measureText()
+     //.putImageData()
+     //.setLineDash()
+     //.setTransform()
+
+/* EXTRA
+ * =====*/
+
+ //.fullRect()
+ //.fullText()
 
 
 
