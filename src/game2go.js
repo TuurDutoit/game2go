e0c18859085fb7b99839c5a5a55e1e8da6b4bba6
TuurDutoit <tuur.dutoit@live.nl>
2014-04-28 17:51:01 +0200

    game basics

--- src/game2go.js
+++ src/game2go.js
@@ -4,7 +4,16 @@ var Game;
 
 
 Game = function(elem, options) {
-	
+	var self     = this;
+	self.options = options;
+	self.elem    = elem;
+	self.context = self.elem.getContext("2d");
+	//self.Draw    = new Draw(this.context);
+
+	self.world   = [];
+	self.level   = [];
+	self.buffer  = [];
+
 }
 
 
