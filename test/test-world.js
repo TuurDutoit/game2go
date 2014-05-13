30de5cf46446147cc3690a6c0ed60310c5dfa19f
DualMatrix <piet.goris@gmail.com>
2014-05-13 20:36:15 +0200

    Changed stuff

--- test/test-world.js
+++ test/test-world.js
@@ -1,42 +1,7 @@
 var testWorld = { //World
     name: "Test World",
     Scenes: [ //Scenes
-        { //Scene 1
-            name: "Scene 1",
-            farBackground: null,
-            normalBackground: "background-scene1",
-            foreground: null,
-            Terrain: [
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"]
-            ]
-        },
+        testScene,
         {//Scene 2
             name: "Awesome Battle",
             normalBackground: "background-scene2",
