603e259f36a2fbaf65da6e7425b4a7e799919724
TuurDutoit <tuur.dutoit@live.nl>
2014-05-14 15:39:59 +0200

    Test page with controls

--- test/test-player.js
+++ test/test-player.js
@@ -1,10 +1,37 @@
-var jumping = false;
-setTimeout(function() {
-	jumping = true;
-}, 1000);
-setTimeout(function() {
-	jumping = false;
-}, 2000);
+var right = false, left = false, up = false, down = false;
+window.addEventListener("keydown", function(e) {
+	switch(e.keyCode) {
+		case 40:
+			down = true;
+			break;
+		case 39:
+			right = true;
+			break;
+		case 38:
+			up = true;
+			break;
+		case 37:
+			left = true;
+			break;
+	}
+});
+window.addEventListener("keyup", function(e) {
+	switch(e.keyCode) {
+		case 40:
+			down = false;
+			break;
+		case 39:
+			right = false;
+			break;
+		case 38:
+			up = false;
+			break;
+		case 37:
+			left = false;
+			break;
+	}
+})
+
 
 
 var testPlayer = {
@@ -13,8 +40,18 @@ var testPlayer = {
 	width: 35,
 	height: 70,
 	update: function(game) {
-		game.offsetX += 2;
-		if(jumping) game.offsetY += 1;
+		if(right) {
+			game.offsetX += 2;
+		}
+		if(left) {
+			game.offsetX -= 2;
+		}
+		if(up) {
+			game.offsetY += 1;
+		}
+		if(down) {
+			game.offsetY -= 1;
+		}
 	},
 	draw: function(d) {
 		d.fillStyle("#000000").fillRect(0, 0, 35, 70);
