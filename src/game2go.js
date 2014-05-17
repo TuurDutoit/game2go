da665119a43930a63e1bf528320cd650d59a8177
DualMatrix <piet.goris@gmail.com>
2014-05-17 13:52:25 +0200

    Fixed some object things

--- src/game2go.js
+++ src/game2go.js
@@ -150,8 +150,8 @@ Game.prototype.drawObjects = function() {
 	for(var i = 0, len = objects.length; i < len; i++) {
         if(objects[i].Draw) {
             var object = objects[i];
-            this.Draw.offsetX = object.positionX - this.offsetX;
-            this.Draw.offsetY = object.positionY - this.offsetY;
+            this.Draw.offsetX = -this.offsetX + object.positionX
+            this.Draw.offsetY =  this.offsetY + object.positionY;
             objects[i].Draw(this.Draw);
         }
 	}
