70510ff1463ad8560950ecb9ac92ceb31803346a
DualMatrix <piet.goris@gmail.com>
2014-05-17 18:52:10 +0200

    Added A Fireball

--- test/test-objects.js
+++ test/test-objects.js
@@ -14,7 +14,27 @@ ObjectTest.prototype.Draw = function(d) {
 	d.drawImage(document.getElementById(this.spriteID), 0, 0);
 }
 
-
+var ObjectFireBall = function(x, y , angle){
+	this.spriteID = "image";
+	this.positionX = x;
+	this.positionY = y;
+	this.angle = angle;
+	return this;
+}
+ObjectFireBall.prototype.Init = function() {
+	this.angleRadian = (this.angle * Math.PI)/180;
+	//alert(this.angle);
+	//alert("Test");
+	this.plusX = Math.cos(this.angleRadian);
+	this.plusY = Math.sin(this.angleRadian);
+}
+ObjectFireBall.prototype.Update = function() {
+	this.positionX += this.plusX;
+	this.positionY -= this.plusY;
+}
+ObjectFireBall.prototype.Draw = function(d) {
+	d.drawImage(document.getElementById(this.spriteID), 0, 0);
+}
 
 var ObjectTestNoOffset = function() {
 	this.spriteID = "image";
