2ce73bd221f6f7f8ce0e92f790dc0b64296096bf
DualMatrix <piet.goris@gmail.com>
2014-05-26 21:26:48 +0200

    Added new scene1

--- test/test-scene.js
+++ test/test-scene.js
@@ -25,3 +25,39 @@ var testScene = {
         [ "Bricks", "Bricks", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks"]
     ]
 }
+var testSceneB = {
+    name: "Scene 2",
+    Backgrounds: [
+        {
+            height: 420, width: 840, positionX: 0, positionY: 0,
+            Draw: function(d) {
+                d.drawImage(document.getElementById("spritesheet"), 0, 0);
+            }
+        }
+    ],
+	Objects: [new ObjectTest(), new ObjectTestNoOffset(), new ObjectFireBall({x:50, y:300, speed:1.5, angle:0, size:32}), new ObjectFireBall({x:90, y:300, speed:1, angle:0, size:32})],
+    Foregrounds: [],
+    Terrain: [
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Empty", "Empty", "Empty", "Bricks", "Bricks" , "Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty","Empty", "Empty", "Empty", "Empty", "Empty", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Bricks","Bricks","Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Bricks","Empty","Empty", "Empty", "Empty", "Empty", "Bricks", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Empty","Empty","Empty", "Empty", "Empty", "Empty", "Empty", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Empty","Empty","Empty", "Bricks", "Empty", "Empty", "Empty", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Empty","Empty","Empty", "Bricks", "Empty", "Empty", "Empty", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Empty","Empty","Empty", "Bricks", "Empty", "Empty", "Empty", "Bricks", "Bricks"],
+        [ "Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks","Bricks", "Bricks", "Bricks", "Bricks", "Bricks", "Empty","Empty","Empty", "Bricks", "Empty", "Empty", "Empty", "Bricks", "Bricks"]
+    ]
+}
