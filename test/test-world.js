b4c3eb8d39ebfcff0984efdf28115bd9a26e5ac8
DualMatrix <piet.goris@gmail.com>
2014-05-26 21:29:33 +0200

    Added new scene2

--- test/test-world.js
+++ test/test-world.js
@@ -2,39 +2,6 @@ var testWorld = { //World
     name: "Test World",
     Scenes: [ //Scenes
         testScene,
-        {//Scene 2
-            name: "Awesome Battle",
-            normalBackground: "background-scene2",
-            Terrain: [
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"],
-                [ "Test", "TestB", "TestB", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestA" ],
-                [ "TestA", "TestA", "TestB"],
-                [ "Test", "Test", "TestB", "TestA"]
-            ]
-        }
+        testSceneB
     ]
 }
